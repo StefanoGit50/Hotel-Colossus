82e1a9ba190149f1d2395d05c3d6d99b36904542
StefanoGit50 <s.santoro41@studenti.unisa.it>
2025-12-18 18:39:05 +0100

    code : fixed RMI

--- src/main/java/it/unisa/gestionePrenotazioni/gestioneCamere/GestioneCamere.java
+++ src/main/java/it/unisa/gestionePrenotazioni/gestioneCamere/GestioneCamere.java
@@ -4,6 +4,8 @@ import it.unisa.gestionePrenotazioni.clients.GestioneCamereInterface;
 
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +15,13 @@ public class GestioneCamere extends UnicastRemoteObject implements GestioneCamer
 {
     private static final long serialVersionUID = -34234234L;
     static Logger logger = Logger.getLogger("global");
-    
+    private static final int RMI_PORT = 1099;
+
     private static List<Stanza> stanze = new ArrayList<>();
-    
+
     public GestioneCamere() throws RemoteException {}
-    
-    @Override 
+
+    @Override
     public Stanza getStanza(int numero)
     {
         for(Stanza s: stanze)
@@ -28,14 +31,14 @@ public class GestioneCamere extends UnicastRemoteObject implements GestioneCamer
         }
         return new Stanza();
     }
-    
-    @Override 
+
+    @Override
     public List<Stanza> getStanze()
     {
         return stanze;
     }
-    
-    @Override 
+
+    @Override
     public void setOccupataGlobale(Stanza s)
     {
         for(Stanza s1: getStanze())
@@ -44,8 +47,8 @@ public class GestioneCamere extends UnicastRemoteObject implements GestioneCamer
                 if(s1.getStatoGlobale().equals("libera"))
                     s1.setStatoGlobale("occupata");
         }
-    }  
-            
+    }
+
     @Override
     public void setLiberaGlobale(Stanza s)
     {
@@ -55,8 +58,8 @@ public class GestioneCamere extends UnicastRemoteObject implements GestioneCamer
                 s.setStatoGlobale("libera");
         }
     }
-    
-    @Override 
+
+    @Override
     public void setOccupataPulizie(Stanza s)
     {
         for(Stanza s1: getStanze())
@@ -64,8 +67,8 @@ public class GestioneCamere extends UnicastRemoteObject implements GestioneCamer
             if(s.getStatoPulizie().equals("libera"))
                 s.setStatoPulizie("occupata");
         }
-    }  
-            
+    }
+
     @Override
     public void setLiberaPulizie(Stanza s)
     {
@@ -75,32 +78,51 @@ public class GestioneCamere extends UnicastRemoteObject implements GestioneCamer
                 s.setStatoPulizie("libera");
         }
     }
-    
+
     public static void main(String[] args) throws RemoteException
-    {   
+    {
         logger.info("Ottengo le camere...");
-        
+
         for(int i=101; i<600; i++)
         {
             stanze.add(new Stanza(i));
         }
-        
+
         logger.info("Camere ottenute!");
-        
+
         try
         {
+            // IMPORTANTE: Avvia l'RMI registry programmaticamente
+            logger.info("Avvio RMI Registry sulla porta " + RMI_PORT + "...");
+            Registry registry;
+            try {
+                // Prova a creare un nuovo registry
+                registry = LocateRegistry.createRegistry(RMI_PORT);
+                logger.info("RMI Registry creato con successo!");
+            } catch (RemoteException e) {
+                // Se esiste già, ottieni il riferimento
+                registry = LocateRegistry.getRegistry(RMI_PORT);
+                logger.info("Connesso a RMI Registry esistente");
+            }
+
             logger.info("Sto creando il gestore camere...");
             GestioneCamere gc = new GestioneCamere();
             logger.info("Gestore camere creato... ");
+
             logger.info("Effettuo il rebind del gestore camere...");
-            Naming.rebind("GestoreCamere", gc);
-            logger.info("Il gestore camere è pronto...");
+            Naming.rebind("rmi://localhost:" + RMI_PORT + "/GestoreCamere", gc);
+            logger.info("Il gestore camere è pronto e registrato!");
+            logger.info("Server in attesa di connessioni...");
+
+            // Mantieni il server in esecuzione
+            Thread.currentThread().join();
         }
         catch(Exception e)
         {
+            logger.severe("Errore durante l'avvio del server RMI: " + e.getMessage());
             e.printStackTrace();
         }
-        
+
     }
-    
+
 }
