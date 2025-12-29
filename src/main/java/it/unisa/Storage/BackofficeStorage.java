8223175f823b0ef52350a58356241691afb5cce0
Renato <renato.natale.2004@gmail.com>
2025-12-29 20:52:24 +0100

    CODE: "Caricamento del file BackOfficeStorage.java (nell package Storage)"

--- src/main/java/it/unisa/Storage/BackofficeStorage.java
+++ src/main/java/it/unisa/Storage/BackofficeStorage.java
@@ -1,16 +1,5 @@
 package it.unisa.Storage;
 
-import java.sql.SQLException;
-import java.util.Collection;
+public class BackOfficeStorage {
 
-/**
- * Questa interfaccia serve per il caricamento dei dati/Cancellazione/Aggiornamento dei
- * dati nel database oppure nel recupero delle informazioni nel DB di HotelCollosus
- * @param <T>
- */
-public interface BackofficeStorage<T>{
-    void doSave(T o) throws SQLException;
-    void doDelete(T o) throws SQLException;
-    T doRetriveByKey(int index) throws SQLException;
-    Collection<T> doRetriveAll(String order) throws SQLException;
 }
